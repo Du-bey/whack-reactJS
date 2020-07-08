9fabb1c2a8bdb91db158e44479835139cec9ca8b
Ritwik Sharma <sharmaritwik98@gmail.com>
2020-07-08 15:45:00 +0530

    d3

--- src/contexts/GlobalState.js
+++ src/contexts/GlobalState.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect } from "react";
-import {csv, contourDensity} from 'd3';
-import {parse} from "papaparse";
-import { a, rec } from './trialread.js';
+import { csv, contourDensity } from "d3";
+import { parse } from "papaparse";
+import { a, rec } from "./trialread.js";
 export const GlobalState = createContext();
 // export const GlobalStateProvider = (props) => {
 //     const [transactions, setTransactions] = useState(
@@ -24,24 +24,24 @@ export const GlobalState = createContext();
 // };
 
 export const GlobalStateProvider = (props) => {
-  const data = parse(a, {header:true});
-  const sugg = parse(rec, {header:true});
-  const[user,setUser]=useState(0);
+  const data = parse(a, { header: true });
+  const sugg = parse(rec, { header: true });
+  const [user, setUser] = useState(0);
   // console.log(sugg.data[user].items);
   const itemdata = sugg.data[user].items;
-  const[suggprods,setsuggprods]=useState(itemdata.split(","));
+  const [suggprods, setsuggprods] = useState(itemdata.split(","));
   const [prods, setProds] = useState([]);
   const [cartProds, setcartProds] = useState([]);
-  const [billamount,setbillamount]=useState(0);
+  const [billamount, setbillamount] = useState(0);
   // const [data,setData]=useState();
   const addProd = (prod) => {
     console.log(prod);
-    setProds([...prods.filter((item) => item.id !== prod.id),prod]);
-    console.log(prods)
+    setProds([...prods.filter((item) => item.id !== prod.id), prod]);
+    console.log(prods);
   };
   const addCartProd = (prod) => {
     setcartProds([...cartProds, prod]);
-    const c = billamount +  + Math.abs(prod.price);
+    const c = billamount + +Math.abs(prod.price);
     setbillamount(c);
     console.log(billamount);
   };
@@ -49,15 +49,25 @@ export const GlobalStateProvider = (props) => {
     setProds([...prods.filter((prods) => prods.id !== id)]);
   };
   const delCartProd = (id) => {
-    const x = [...cartProds.filter((prods) => prods.id === id)]
+    const x = [...cartProds.filter((prods) => prods.id === id)];
     setcartProds([...cartProds.filter((prods) => prods.id !== id)]);
-    console.log(x.price)
+    console.log(x.price);
     const c = Math.abs(billamount) - Math.abs(x[0].price);
     setbillamount(Math.abs(c));
   };
   return (
     <GlobalState.Provider
-      value={{ prods: prods, addProd: addProd, delProd: delProd, cartProds: cartProds, addCartProd: addCartProd, delCartProd: delCartProd, suggprods:suggprods, data:data, billamount:billamount}}
+      value={{
+        prods: prods,
+        addProd: addProd,
+        delProd: delProd,
+        cartProds: cartProds,
+        addCartProd: addCartProd,
+        delCartProd: delCartProd,
+        suggprods: suggprods,
+        data: data,
+        billamount: billamount,
+      }}
     >
       {props.children}
     </GlobalState.Provider>
